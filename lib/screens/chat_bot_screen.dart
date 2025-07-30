0060c8ec7a6108ba9cdccf384f1967686771bfc9
Piyush Pattanayak <piyushpattanayak10@gmail.com>
2025-07-30 23:27:57 +0530

    Update chat_bot_screen.dart

--- lib/screens/chat_bot_screen.dart
+++ lib/screens/chat_bot_screen.dart
@@ -17,7 +17,7 @@ class _ChatBotScreenState extends State<ChatBotScreen> {
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..loadRequest(
-        Uri.parse('https://cdn.botpress.cloud/webchat/v3.0/shareable.html?configUrl=https://files.bpcontent.cloud/2025/06/13/16/20250613164208-QX9E9CDH.json'),
+        Uri.parse('https://uniconnect-rltc.onrender.com/'),
       );
   }
 
